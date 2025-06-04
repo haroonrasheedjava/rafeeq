53582621a6b71f61c9870a1add144fe3d28e31f6
haroonrasheedjava <haroonrasheedjava@gmail.com>
2025-06-04 13:53:23 +0300

    Fix GitHub spelling

--- example/simple.py
+++ example/simple.py
@@ -5,7 +5,7 @@ from dia.model import Dia
 
 model = Dia.from_pretrained("nari-labs/Dia-1.6B")
 
-text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
+text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on GitHub or Hugging Face."
 
 output = model.generate(text)
 
